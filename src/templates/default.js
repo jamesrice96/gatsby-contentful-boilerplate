a8703504035f2e7f6d4905cf28eb9e0dbd77a7be
James Rice <james@purplecs.com>
2019-05-14 16:55:47 +0100

    minor text changes

--- src/templates/default.js
+++ src/templates/default.js
@@ -9,9 +9,16 @@ export default ({ data }) => {
     <>
       <Header />
       <h1>Hello from the default template</h1>
-      <p>{page.title}</p>
-      <p>{page.pageTitleSeo}</p>
-      <p>{page.pageDescriptionSeo}</p>
+      <p>
+        <b>{page.title}</b> has been dynamically created from the contentful
+        api.
+      </p>
+      <p>
+        <b>Page title:</b> {page.pageTitleSeo}
+      </p>
+      <p>
+        <b>Page description:</b> {page.pageDescriptionSeo}
+      </p>
     </>
   );
 };

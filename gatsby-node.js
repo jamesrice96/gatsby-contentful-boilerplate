fa316ad66af0d5db54e2f999de07ba636cfbd365
James Rice <james@purplecs.com>
2019-05-14 13:10:36 +0100

    added a basic implementation of the createPages API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,26 @@
 exports.onCreateNode = ({ node }) => {
-  //We are specifically looking for a page node
   if (node.internal.type === 'ContentfulPage') {
     console.log('pages fetching', node.internal.type);
   }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  return graphql(`
+    {
+      allContentfulPage {
+        edges {
+          node {
+            title
+            slug
+            pageTitleSeo
+            pageDescriptionSeo
+          }
+        }
+      }
+    }
+  `).then(result => {
+    console.log(JSON.stringify(result, null, 4));
+  });
 };
